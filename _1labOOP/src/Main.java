960cc343c8f900f7ffeec027c9f3d737197926c8
RoGad <83944231+RoGad@users.noreply.github.com>
2023-10-16 00:12:15 +0600

    Add files via upload

---
+++ _1labOOP/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Введите текст");
+        Scanner in = new Scanner(System.in);
+        String string = in.nextLine();
+        String[] words = string.split("\\s+");
+        HashMap<String, Integer> wordToCount = new HashMap<>();
+        for (String word : words)
+        {
+            if (!wordToCount.containsKey(word))
+            {
+                wordToCount.put(word, 0);
+            }
+            wordToCount.put(word, wordToCount.get(word) + 1);
+        }
+        for (String word : wordToCount.keySet())
+        {
+            System.out.println(word + " " + wordToCount.get(word));
+        }
+    }
+}
